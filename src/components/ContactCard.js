2424318e0c7a9e3563878868019d02baadeaf65e
jonathan <toleto2013@gmail.com>
2020-10-04 11:10:08 -0400

    added contact

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,6 +1,30 @@
-// import React from "react";
-// import ContactPNG from "../assets/contact.png"
+import React from "react";
+import ContactPNG from "../assets/contact.png"
+import Contact from "./pages/Contact";
 
 
-// function ContactCard(props) {
-//     return (
+function ContactCard(props) {
+    return (
+    <div className="contact-card">
+
+    <div className="card">
+        <h1 className="text-center">Contact Me</h1>
+        <div className="card-content">
+            <a href="tel:+7043077417" className="ml-4 mt-4 font-weight-bold">Phone</a>
+
+            <a href="mailto: toleto2013@gmail.com" className="ml-4 mt-4 font-weight-bold">Email</a>
+
+            <a href="https://www.linkedin.com/in/jonathan-de-los-santos-abb7081a6/" className="ml-4 mt-4 font-weight-bold">LinkedIn</a>
+
+            <a href="https://github.com/jdelossantos47" className="ml-4 mt-4 font-weight-bold">GitHub</a>
+
+            <img src={Contact.png} alt="" className="profile float-left"></img>
+        </div>
+     </div>
+    </div>
+
+);
+
+}
+
+export default ContactCard;
